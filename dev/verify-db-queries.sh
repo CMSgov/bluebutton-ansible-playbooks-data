39cd80b2bca6cf3f37dcce52a590c652a0c6c3aa
Jacob Geer <jacob.geer@gdit.com>
2019-08-16 11:46:35 -0500

    Fixing syntax for assignment statements

--- dev/verify-db-queries.sh
+++ dev/verify-db-queries.sh
@@ -4,16 +4,16 @@
 # "/bluebutton-ansible-playbooks-data/dev/database-queries.txt") for 
 # a random number of beneficiaryIds.
 #
-# 	$ verify-db-queries.sh <database-name> <username> <number-of-randomb-benes>#
+# 	$ verify-db-queries.sh <database-name> <username> <number-of-randomb-benes>
 #
 
-DBNAME= $1
-USERNAME= $2
-NUMBENESTOQUERY= $3
-databaseQueries= "/bluebutton-ansible-playbooks-data/dev/database-queries.txt"
+DBNAME=$1
+USERNAME=$2
+NUMBENESTOQUERY=$3
+databaseQueries="/bluebutton-ansible-playbooks-data/dev/database-queries.txt"
 
 # select random sample of Beneficiaries.beneficiaryId
-randomBeneIds= `psql -U $USERNAME -d $DBNAME -c "SELECT beneficiaryId FROM Beneficiaries ORDER BY RANDOM() LIMIT $NUMBENESTOQUERY"`
+randomBeneIds=`psql -U $USERNAME -d $DBNAME -c "SELECT beneficiaryId FROM Beneficiaries ORDER BY RANDOM() LIMIT $NUMBENESTOQUERY"`
 
 # set queries to timeout after 15 seconds
 setTimeout() {
@@ -25,31 +25,31 @@ checkForTimeout() {
 	# FIXME exit status of 2 might be for timeouts - need to verify
 	if [ echo $? != 0]
 	then
-		$timedout= true
+		$timedout=true
 	fi
 }
 
 runPsqlQuery() {
-	dbQuery= $1
+	dbQuery=$1
 	psql -U $USERNAME -d $DBNAME $dbQuery
 }
 
-timedout= false
-numberOfTimeouts= 0
+timedout=false
+numberOfTimeouts=0
 for beneId in $randomBeneIds
 do
 	echo "Running all DB queries for beneId = $beneId"
 	
 	# get the hicn and claimIds for this beneId since it is needed for other queries
-	hicn= `psql -U $USERNAME -d $DBNAME -c "SELECT hicn FROM Beneficiaries WHERE beneficiaryId = $beneId"`
-	carrClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM CarrierClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	dmeClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM DMEClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	hhaClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM HHAClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	hospiceClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM HospiceClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	inpatientClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM InpatientClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	outpatientClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM OutpatientClaims WHERE beneficiaryId = $beneId" LIMIT 1`
-	pdeEventId= `psql -U $USERNAME -d $DBNAME -c "SELECT eventId FROM PartDEvents WHERE beneficiaryId = $beneId" LIMIT 1`
-	snfClaimId= `psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM SNFClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	hicn=`psql -U $USERNAME -d $DBNAME -c "SELECT hicn FROM Beneficiaries WHERE beneficiaryId = $beneId"`
+	carrClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM CarrierClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	dmeClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM DMEClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	hhaClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM HHAClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	hospiceClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM HospiceClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	inpatientClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM InpatientClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	outpatientClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM OutpatientClaims WHERE beneficiaryId = $beneId" LIMIT 1`
+	pdeEventId=`psql -U $USERNAME -d $DBNAME -c "SELECT eventId FROM PartDEvents WHERE beneficiaryId = $beneId" LIMIT 1`
+	snfClaimId=`psql -U $USERNAME -d $DBNAME -c "SELECT claimId FROM SNFClaims WHERE beneficiaryId = $beneId" LIMIT 1`
 	
 	# run all DB queries once for the current beneId
 	while IFS= read -r line
@@ -62,7 +62,7 @@ do
 	if [$timedout]
 	then
 		((numberOfTimeouts++))
-		$timedout= false
+		$timedout=false
 	fi
 done
 
